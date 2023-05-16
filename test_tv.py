b6f8bdfc209720ed348605cfedae4bbf77106504
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-17 07:18:32 +0800

    Pseudocode

---
+++ test_tv.py
@@ -0,0 +1,12 @@
+# create a class named "TV"
+# create a constructor to initialize the objects
+# create a method that will indicate if the TV is on/off
+# create a method that will indicate the channel of Tv
+# create a method that will set the channel of Tv
+# create a method that will get the voulume level of the TV
+# create a method that will set a new volume for the tv
+# create a method that will increase the channel number by 1
+# create a method that will decrease the channel number by 1
+# create a method that will increase the volume level by 1
+# create a method that will decrease the volume level by 1
+# create two objects that satisfy the following arguments in the constructor
