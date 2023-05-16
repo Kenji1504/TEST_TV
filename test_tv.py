7beda3544f78cefff6ae42680d942412603791a9
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-17 07:22:05 +0800

    creating a class

--- test_tv.py
+++ test_tv.py
@@ -1,5 +1,12 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# This is an object-oriented program that tests particular functions of a TV.
+
+
 # create a class named "TV"
-# create a constructor to initialize the objects
+class TV:
+    # create a constructor to initialize the objects
+    
 # create a method that will indicate if the TV is on/off
 # create a method that will indicate the channel of Tv
 # create a method that will set the channel of Tv
