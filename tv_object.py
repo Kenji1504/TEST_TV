8015c0d3551586611b85174452cab0037854dca4
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-17 09:02:37 +0800

    creating two objects

---
+++ tv_object.py
@@ -0,0 +1,6 @@
+# from test_tv.py, import class TV
+from test_tv import TV
+
+# create two objects that satisfy the following arguments in the constructor
+tv_1 = TV("tv1", True, 118, 5)
+tv_2 = TV("tv2", True, 3, 2)
