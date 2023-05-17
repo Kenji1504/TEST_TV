abab2abae377d294ed85a0fae9d335a139e743a4
Kenji1504 <gamboakenleam1015@gmail.com>
2023-05-17 08:04:39 +0800

    to show the volume level

--- test_tv.py
+++ test_tv.py
@@ -25,7 +25,8 @@ class TV:
     def set_channel(self, new_channel):
         self.channel = new_channel
     # create a method that will get the voulume level of the TV
-    
+    def show_volume(self):
+        print(self.name, "is set on volume level", self.volume)
 # create a method that will set a new volume for the tv
 # create a method that will increase the channel number by 1
 # create a method that will decrease the channel number by 1
